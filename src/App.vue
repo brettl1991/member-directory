6ca3fa7bc9a6500a28a37b51f0bbaf7c352f0f57
Agnes Brettl <brettl1991@gmail.com>
2026-03-30 10:33:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from "vue";
+const title = ref("Team Directory");
+</script>
+<template>
+  <main>
+    <header>
+      <h1>{{ title }}</h1>
+    </header>
+  </main>
+</template>
